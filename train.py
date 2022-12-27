ea752d5389ee0486bcc6444bf8708056386c09d3
sachidanand <ep19b010@smail.iitm.ac.in>
2022-12-27 13:20:18 +0530

    trained resnet on syn data

--- train.py
+++ train.py
@@ -13,8 +13,12 @@ import json
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 train_json_path = "./demo_dataset/train.json"
+valid_json_path = "./demo_dataset/validation.json"
 test_json_path = "./demo_dataset/test.json"
 mean_std_path = "./mean_std.json"
+
+batch_size = 32
+save_model_path = "./weights/model.pth"
 
 def generate_dataset(data_path,batch_size=32,shuffle=True):
 
@@ -30,42 +34,57 @@ def generate_dataset(data_path,batch_size=32,shuffle=True):
     dataset = SyntheticDataset(data_path, transform)
     return dataset
 
-model = ResNet18(12).to(device)
+model = ResNet18(12,True).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 train_dataset = generate_dataset(train_json_path)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True,num_workers=2)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,num_workers=2)
+valid_dataset = generate_dataset(valid_json_path)
+valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True,num_workers=2)
 test_dataset = generate_dataset(test_json_path)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True,num_workers=2)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True,num_workers=2)
 
-def train(model, train_loader, test_loader,criterion,optimizer,num_epochs=10):
-    for epoch in range(num_epochs):
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            inputs, labels = data["image"].to(device), data["label"].to(device)
+def train(model, train_loader, valid_loader, criterion, optimizer, epochs=10):
+    for epoch in range(epochs):
+        model.train()
+        for i, data in enumerate(train_loader):
+            images, labels = data["image"].to(device), data["label"].to(device)
             optimizer.zero_grad()
-            outputs = model(inputs)
+            outputs = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            running_loss += loss.item()
-            if i % 200 == 199:
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 200))
-                running_loss = 0.0
+            if i % 100 == 0:
+                print("Epoch: {}/{}, Step: {}/{}, Loss: {}".format(epoch+1, epochs, i, len(train_loader), loss.item()))
+        model.eval()
         correct = 0
         total = 0
         with torch.no_grad():
-            for data in test_loader:
+            for data in valid_loader:
                 images, labels = data["image"].to(device), data["label"].to(device)
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-        print('Accuracy of the network on the test images: %d %%' % (
-            100 * correct / total))
-    print('Finished Training')
+        print("Accuracy of the network on the validation images: {}%".format(100 * correct / total))
 
-train(model, train_loader, test_loader,criterion,optimizer)
-    
+train(model, train_loader, valid_loader, criterion, optimizer, epochs=10)
+
+def test(model, test_loader):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data["image"].to(device), data["label"].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print("Accuracy of the network on the test images: {}%".format(100 * correct / total))
+
+test(model, test_loader)
+
+# save model
+torch.save(model.state_dict(), save_model_path)
